6a5dbf807a44c38942dfa78c0c632b40b1b72feb
pl1998 <2139714249#qq.com>
2023-10-29 12:19:26 +0800

    Update:main.yml

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 cd ./laravel-tests
-sudo chmod -R 777  ./storage
-sudo chmod -R 777  ./bootstrap/cache
-php artisan serve --port=8000 --host=0.0.0.0 > /dev/null 2>&1 &
+export DISPLAY=:99.0
+sudo chmod -R 777  storage
+sudo chmod -R 777  bootstrap/cache
+php artisan serve --port=8000 > /dev/null 2>&1 &
+cd storage/logs
+cat laravel.log
+
