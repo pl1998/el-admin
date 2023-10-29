f578c9a0640078ab30cf925c09b5a124327da469
latent <panliang@mythinkcar.com>
2023-10-29 23:01:43 +0800

    Update:main.yml

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cp -f ./.env.testing ./laravel-tests/.env
 cp -f ./tests/.env.testing ./laravel-tests/.env
 cd ./laravel-tests
 git clone https://github.com/pl1998/el-admin.git
@@ -7,5 +6,6 @@ composer config repositories.latent path el-admin
 composer require latent/el-admin:dev-master
 php artisan vendor:publish --provider="Latent\ElAdmin\ElAdminServiceProvider"  --force
 php artisan el-admin:install
-cd ./laravel-tests/el-admin
+ls -a
+cd ./laravel-tests/el-admin/
 composer install
