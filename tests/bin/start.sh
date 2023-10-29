c144be022bf7307ef7139abf5c04ee285e917c5e
latent <panliang@mythinkcar.com>
2023-10-30 00:22:14 +0800

    fix;

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -4,5 +4,5 @@ cd ./laravel-tests
 export DISPLAY=:99.0
 sudo chmod -R 777  storage
 sudo chmod -R 777  bootstrap/cache
-php artisan serve --port=8000 > /dev/null 2>&1 &
+php artisan serve --port=8001 > /dev/null 2>&1 &
 
