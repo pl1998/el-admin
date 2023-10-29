e6ef1e1d9f1e43220fa31ba83acc8dca212c5d42
latent <panliang@mythinkcar.com>
2023-10-29 23:38:07 +0800

    Update:yml

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ./laravel-tests
-sudo chmod -R 777  storage
-sudo chmod -R 777  bootstrap/cache
+sudo chmod -R 777  ./storage
+sudo chmod -R 777  ./bootstrap/cache
 php artisan serve --port=8000  > /dev/null 2>&1 &
 lsof -i:8000
