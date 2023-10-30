a221719214fcdffec2f5f40b0f4fbd5982fd49e0
pl1998 <2139714249#qq.com>
2023-10-30 21:16:47 +0800

    update:sh

--- tests/bin/start.sh
+++ tests/bin/start.sh
@@ -4,5 +4,6 @@ cd ./laravel-tests
 export DISPLAY=:99.0
 sudo chmod -R 0755  storage
 sudo chmod -R 0755  bootstrap/cache
-php artisan serve --port=8300 > /dev/null 2>&1 &
-
+sudo ls -la
+sudo php artisan serve --port=8300 > /dev/null 2>&1 &
+sudo ps -ef | grep php
