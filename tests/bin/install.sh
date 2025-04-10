1d4a2715d2ddab3ec552d30bb13b9aab7227c9c1
pl1998 <2139714249@qq.com>
2025-04-10 12:05:44 +0800

    update:action yml

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -28,8 +28,8 @@ echo "Version: $VERSION"
 cp -f ./tests/.env.testing ./laravel-tests/.env
 cd ./laravel-tests
 git clone https://github.com/pl1998/el-admin.git
-composer config repositories.latent path "$VERSION"
-composer require latent/el-admin:dev-master
+composer config repositories.latent path
+composer require latent/el-admin "$VERSION"
 php artisan vendor:publish --provider="Latent\ElAdmin\ElAdminServiceProvider"  --force
 php artisan el-admin:install
 cd el-admin
