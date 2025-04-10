eec5cad5d36be1e17a7393bcf377c99f43c71560
pl1998 <2139714249@qq.com>
2025-04-10 11:43:37 +0800

    update:action yml

--- tests/bin/install.sh
+++ tests/bin/install.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
+
+# 解析参数
+# 解析参数
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --version=*)
+      VERSION="${1#*=}"
+      ;;
+    --module=*)
+      MODULE="${1#*=}"
+      ;;
+    --env=*)
+      ENV_FILE="${1#*=}"
+      ;;
+    --with-features=*)
+      FEATURES="${1#*=}"
+      ;;
+    *)
+      echo "Unknown parameter: $1"
+      exit 1
+  esac
+  shift
+done
+VERSION=${VERSION:-"v1.0.1"}
+echo "Version: $VERSION"
+
 cp -f ./tests/.env.testing ./laravel-tests/.env
 cd ./laravel-tests
 git clone https://github.com/pl1998/el-admin.git
-composer config repositories.latent path el-admin
+composer config repositories.latent path "$VERSION"
 composer require latent/el-admin:dev-master
 php artisan vendor:publish --provider="Latent\ElAdmin\ElAdminServiceProvider"  --force
 php artisan el-admin:install
